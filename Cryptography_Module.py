b6a52cde351177d3b6ae84679c99d8ea66f41e87
LittlePIP <phelipelipeoliveura@gmail.com>
2021-07-05 11:25:28 -0300

    Modulo criptografia Hello World

---
+++ Cryptography_Module.py
@@ -0,0 +1,72 @@
+from string import ascii_lowercase
+from random import randint, choice
+
+
+alfabeto = list(ascii_lowercase)
+
+
+# Gera chave pseudo aleatoria para criptografia
+def __gerar_chave(num=int()):
+    tamanho = randint(num, num + 100)
+    chave = [choice(alfabeto) for _ in range(0, tamanho)]
+    return "".join(chave)
+
+
+# Criptografa usando uma chave pseudo aleatoria a frase colocada no parametro.
+# Retorna a frase criptografada com a chave em questão
+def criptografar(word=str(), la_chave="None"):
+    if la_chave != "None":
+        key = la_chave
+    else:
+        key = __gerar_chave(len(word))
+    if len(key) < len(word):
+        return "Erro a chave deve ser '=' ou '>' que a frase!"
+    else:
+        cripto_array = list()
+        key_array = list()
+        for letter in range(0, len(word)):
+            if not word[letter] in alfabeto:
+                cripto_array.append(str(word[letter]))
+            else:
+                cripto_array.append(alfabeto.index(word[letter]))
+                key_array.append(alfabeto.index(key[letter]))
+        return __transformador(cripto_array, key_array)
+
+
+# Criptografa a frase em questão gerando o alfabeto de vigenere
+def __transformador(palavra, indexkey):
+    cripto = list()
+    indexx = 0
+    for numero, local in enumerate(palavra):
+        if type(local) != int:
+            cripto.append(local)
+        else:
+            gerador = alfabeto[local:]
+            total = 26 - len(gerador)
+            for num in range(0, total):
+                gerador.append(alfabeto[num])
+            cripto.append(gerador[indexkey[indexx]])
+            indexx += 1
+    chave = [f"{str(a)}-" for a in indexkey]
+    return "".join(cripto), "".join(chave)
+
+
+# Descriptografa a cifra de vigenere e retorna a frase descriptografada
+def descripto(word=str(), passw=str()):
+    final = list()
+    tamanho = 0
+    password = passw.split("-")
+    password.pop()
+    for letra in word:
+        if not letra in alfabeto:
+            final.append(letra)
+        else:
+            equivale = alfabeto.index(letra) - int(password[tamanho])
+            if equivale < 0:
+                equivale = 26 + equivale
+                final.append(alfabeto[equivale])
+            else:
+                final.append(alfabeto[equivale])
+            tamanho += 1
+    return "".join(final)
+
