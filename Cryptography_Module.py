35dc9eec9dc484e8cb82d80fd6d24d1141051812
LittleKiwi <phelipe.freire@llk.com.br>
2021-09-04 03:38:32 -0300

    vigenere module v2.0

--- Cryptography_Module.py
+++ Cryptography_Module.py
@@ -1,5 +1,5 @@
 from string import ascii_lowercase
-from random import randint, choice
+from random import randint, choice, triangular
 
 
 alfabeto = list(ascii_lowercase)
@@ -10,27 +10,6 @@ def __gerar_chave(num=int()):
     tamanho = randint(num, num + 100)
     chave = [choice(alfabeto) for _ in range(0, tamanho)]
     return "".join(chave)
-
-
-# Criptografa usando uma chave pseudo aleatoria a frase colocada no parametro.
-# Retorna a frase criptografada com a chave em questão
-def criptografar(word=str(), la_chave="None"):
-    if la_chave != "None":
-        key = la_chave
-    else:
-        key = __gerar_chave(len(word))
-    if len(key) < len(word):
-        return "Erro a chave deve ser '=' ou '>' que a frase!"
-    else:
-        cripto_array = list()
-        key_array = list()
-        for letter in range(0, len(word)):
-            if not word[letter] in alfabeto:
-                cripto_array.append(str(word[letter]))
-            else:
-                cripto_array.append(alfabeto.index(word[letter]))
-                key_array.append(alfabeto.index(key[letter]))
-        return __transformador(cripto_array, key_array)
 
 
 # Criptografa a frase em questão gerando o alfabeto de vigenere
@@ -51,6 +30,44 @@ def __transformador(palavra, indexkey):
     return "".join(cripto), "".join(chave)
 
 
+# Criptografa usando uma chave pseudo aleatoria a frase colocada no parametro.
+# Retorna a frase criptografada com a chave em questão
+def criptografar(word=str(), la_chave="None"):
+    if la_chave != "None":
+        
+        # Calibrando tamanho da chave para tamanho da frase
+        if len(la_chave) == len(word) or len(la_chave) > len(word):
+            key = la_chave
+        
+        # Chave menor que a palavra a ser criptografada
+        elif len(la_chave) < len(word):
+            key = [a for a in la_chave]
+            contagem = 0
+            compensa = len(word) - len(la_chave)
+            for _ in range(0, compensa):
+                key.append(la_chave[contagem])
+                contagem += 1
+                if contagem == len(la_chave):
+                    contagem = 0
+            key = ''.join(key)
+        
+    else:
+        key = __gerar_chave(len(word))
+    
+    cripto_array = list()
+    key_array = list()
+    for letter in range(0, len(word)):
+        if not word[letter] in alfabeto:
+            cripto_array.append(str(word[letter]))
+        else:
+            cripto_array.append(alfabeto.index(word[letter]))
+            key_array.append(alfabeto.index(key[letter]))
+    return __transformador(cripto_array, key_array)
+
+
+
+
+
 # Descriptografa a cifra de vigenere e retorna a frase descriptografada
 def descripto(word=str(), passw=str()):
     final = list()
@@ -69,4 +86,3 @@ def descripto(word=str(), passw=str()):
                 final.append(alfabeto[equivale])
             tamanho += 1
     return "".join(final)
-
